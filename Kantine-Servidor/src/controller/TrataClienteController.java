c99239268cb92c382e5098295d5fa0996ae69ba1
Eduardo Vinícius Faleiro <113296487+eduardovfaleiro@users.noreply.github.com>
2023-09-19 15:02:21 -0300

    Add files via upload

---
+++ Kantine-Servidor/src/controller/TrataClienteController.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author aluno
+ */
+public class TrataClienteController {
+
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private int idUnico;
+    
+    public TrataClienteController(Socket socket, int idUnico){
+        this.socket = socket;
+        this.idUnico = idUnico;
+        
+        try {
+            this.in =  new ObjectInputStream(this.socket.getInputStream());
+            this.out = new ObjectOutputStream(this.socket.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public void run(){
+        String comando; // comando recebido do cliente
+        System.out.println("Esperando comandos do cliente...");
+        
+        try {
+            comando = (String) in.readObject();
+            // enquanto o comando NÃO for fim, fica dentro do looping
+            while (!comando.equalsIgnoreCase("fim")) {                
+                System.out.println("Cliente "+idUnico+" enviou o comando: "+comando);
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                comando = (String) in.readObject();
+            }
+        
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Cliente " +idUnico+ "finalizou a conexão.");
+        try {
+            this.in.close();
+            this.out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+
+    
+}
---
+++ Kantine-Servidor/src/factory/Conector.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author aluno
+ */
+public class Conector {
+    
+    public static Connection getConnection(){
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+            String banco = "bikeshop2";
+            String usuario = "root";
+            String senha = "";
+            
+            return DriverManager.getConnection(url+banco,usuario,senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+}
